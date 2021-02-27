72ba556c53e01a866f070435e6b2cb2185a93d3c
r203323 <kobayashi.reni.ot@tut.jp>
2021-02-27 21:09:27 +0900

    feat: Contributorの詳細を表示する処理の追加

--- app/src/main/java/com/example/github_contributor_search/ui/MainActivity.kt
+++ app/src/main/java/com/example/github_contributor_search/ui/MainActivity.kt
@@ -5,20 +5,33 @@ import android.os.Bundle
 import com.example.github_contributor_search.R
 import com.example.github_contributor_search.service.Contributor
 
+// 表示するFragmentを切り替える
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
-            val fragment = ContributorListFragment()
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.fragment_container, fragment, TAG_OF_PROJECT_LIST_FRAGMENT)
+            // Contributorのリストを表示
+            val contributorListFragment = ContributorListFragment()
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, contributorListFragment, TAG_CONTRIBUTOR_LIST)
                 .commit()
         }
     }
 
-    fun show(contributor: Contributor) {
+    // Contributorの詳細を表示
+    fun showContributorDetail(contributor: Contributor) {
+        val bundle = Bundle()
+        // contributorのLogin名を渡す
+        bundle.putString("LOGIN_NAME", contributor.login)
+
+        val contributorDetailFragment = ContributorDetailFragment()
+        contributorDetailFragment.arguments = bundle
+        supportFragmentManager
+            .beginTransaction()
+            .addToBackStack(null)
+            .replace(R.id.fragment_container, contributorDetailFragment)
+            .commit()
     }
 }
